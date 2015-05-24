ad9de61d36b4b2a7ea9d84a0c4e746570e072204
Jaehwan Kim <jae.hwan.kim@samsung.com>
2015-05-24 17:35:24 +0900

    efl.sh: add wayland environment variable.
    Change-Id: Ic43b7cdbf26caa34a7a43c85beec27f813132cd3

--- profile.d/efl.sh
+++ profile.d/efl.sh
@@ -1,5 +1,6 @@
 ## Elementary
 export ELM_PROFILE="mobile"
+export ELM_DISPLAY="wl"
 export ELM_SCALE=4.0
 
 ## Ecore
@@ -8,3 +9,6 @@ export ECORE_IMF_INPUT_PANEL_ENABLED=1
 ## Evas
 export EVAS_FONT_DPI=72
 export EVAS_GL_NO_BLACKLIST=1
+
+## Wayland
+export WAYLAND_DISPLAY="wayland-0"
